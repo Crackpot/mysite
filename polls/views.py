fd335a9a02cf92e48d63f2e8af84d4bf7ca0e0a2
Crackpot <gaofeitop@163.com>
2021-06-20 20:22:41 +0800

    Finished tutorial03

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,48 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # 打印一句话
+    # return HttpResponse("Hello, world. You're at the polls index.")
+
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # 将几个问题的文字用逗号连接
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # 返回文字
+    # return HttpResponse(output)
+
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # 使用HttpResponse返回内容
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', {'question': question})
+
+    # 打印一句话
+    # return HttpResponse("You're looking at question %s." % question_id)
+
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
